f207b9c16fa63a1762bab219f1db4ac84857de0d
sergiodelatorre369-debug <sergiodelatorre369@gmail.com>
2026-07-19 21:30:43 -0600

    actualizacion de enlaces mediafire

--- frontend/src/config/appConfig.js
+++ frontend/src/config/appConfig.js
@@ -7,15 +7,15 @@
 // ---------------------------------------------------------------------------
 export const DEFAULT_CONFIG = {
   isoOficial: {
-    nombre: "PES PPSSPP Oficial",
-    version: "v1.0",
-    url: "https://ejemplo.com/descargar-iso",
+    nombre: "PES PPSSPP Chelito 19 Oficial",
+    version: "2026",
+    url: "https://www.mediafire.com/file/1driorxdmunof2o/eFootball+2026+PSP+C19+v2+-+Lite+%28+Camara+PS5+%29.iso/file",
   },
   texturas: {
-    url: "https://ejemplo.com/descargar-texturas",
+    url: "https://www.mediafire.com/file/4knd5ryxdy1xur4/C19F01598.zip/file",
   },
   savedata: {
-    url: "https://ejemplo.com/descargar-savedata",
+    url: "https://www.mediafire.com/file/my6f0nqh3afswgq/C19G0159800001000.zip/file",
   },
   online: {
     zeroTierNetworkId: "56374ac9a4e0f579",
