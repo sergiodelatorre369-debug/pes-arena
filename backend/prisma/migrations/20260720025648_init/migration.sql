0e0dfa8995f827238276cda1ed309878022e2059
sergiodelatorre369-debug <sergiodelatorre369@gmail.com>
2026-07-19 21:18:09 -0600

    ACTUALIZACION DATA BASE

---
+++ backend/prisma/migrations/20260720025648_init/migration.sql
@@ -0,0 +1,16 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "passwordHash" TEXT NOT NULL,
+    "countryCode" TEXT,
+    "countryName" TEXT,
+    "countryFlag" TEXT,
+    "points" INTEGER NOT NULL DEFAULT 0,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
